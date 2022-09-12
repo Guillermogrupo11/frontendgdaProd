7b9739cd6cd3f3f2d199adefba6677be331fb0a9
Guillermo <guillermo.arena@gmail.com>
2022-09-12 00:01:08 -0300

    hys

--- FrontendgdaP/src/app/app.module.ts
+++ FrontendgdaP/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {NgCircleProgressModule} from 'ng-circle-progress';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -37,7 +37,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgCircleProgressModule.forRoot({}),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
