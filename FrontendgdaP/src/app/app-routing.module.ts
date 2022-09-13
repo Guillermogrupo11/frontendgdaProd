8414f1e69d469a018e59104c01a6424e99ff4e99
Guillermo <guillermo.arena@gmail.com>
2022-09-12 23:26:02 -0300

    educacion service

--- FrontendgdaP/src/app/app-routing.module.ts
+++ FrontendgdaP/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 const routes: Routes = [{path: '', component: HomeComponent},
 {path: 'login', component: LoginComponent},
 {path: 'nuevaexp', component: NewExperienciaComponent},
-{path: 'editexp', component: EditExperienciaComponent}
+{path: 'editexp/:id', component: EditExperienciaComponent}
 ];
 
 @NgModule({
