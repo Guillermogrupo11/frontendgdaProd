40520ce1791baa641baf0f8881e89cf71f7d5662
Guillermo <guillermo.arena@gmail.com>
2022-09-14 13:42:24 -0300

    arreglos por errores

--- FrontendgdaP/src/app/app-routing.module.ts
+++ FrontendgdaP/src/app/app-routing.module.ts
@@ -9,14 +9,15 @@ import { EditSkillComponent } from './components/hys/edit-skill.component';
 import { NewSkillComponent } from './components/hys/new-skill.component';
 import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [{path: '', component: HomeComponent},
-{path: 'login', component: LoginComponent},
-{path: 'nuevaexp', component: NewExperienciaComponent},
-{path: 'editexp/:id', component: EditExperienciaComponent},
-{path: 'nuevaedu', component: NeweducacionComponent},
-{path: 'editedu/:id', component: EditeducacionComponent},
-{path: 'NewSkillComponent', component: NewSkillComponent},
-{path: 'EditSkillComponent', component: EditSkillComponent},
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'nuevaexp', component: NewExperienciaComponent},
+  { path: 'editexp/:id', component: EditExperienciaComponent},
+  { path: 'nuevaedu', component: NeweducacionComponent},
+  { path: 'editedu/:id', component: EditeducacionComponent},
+  { path: 'newskill', component: NewSkillComponent},
+  { path: 'editskill/:id', component: EditSkillComponent}
 ];
 
 @NgModule({
